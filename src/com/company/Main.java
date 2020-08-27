ca7a0521dae9c71ee2688eaf488cc6ed275b4240
Ivan Dubovoy <dubovoy671@gmail.com>
2020-08-27 15:59:50 +0900

    Fixed methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,83 +6,44 @@ import java.io.IOException;
 
 public class Main {
 
+    public static StringBuilder str = new StringBuilder();
+
     public static void main(String[] args) {
-        StringBuilder str = new StringBuilder();
+        newDir("C://Games", "src");
+        newDir("C://Games", "res");
+        newDir("C://Games", "savegames");
+        newDir("C://Games", "temp");
+        newDir("C://Games//res", "drawables");
+        newDir("C://Games//res", "vectors");
+        newDir("C://Games//res", "icons");
+        newDir("C://Games//src", "main");
+        newDir("C://Games//src", "test");
+        newFile("C://Games//src//main", "Main.java");
+        newFile("C://Games//src//main", "Utils.java");
+        newFile("C://Games//temp", "temp.txt");
 
-        File src = new File("C://Games", "src");
-        if (src.mkdir()) {
-            str.append("Yes\n");
-        }
-
-        File res = new File("C://Games", "res");
-        if (res.mkdir()) {
-            str.append("Yes\n");
-        }
-
-        File savegames = new File("C://Games", "savegames");
-        if (savegames.mkdir()) {
-            str.append("Yes\n");
-        }
-
-        File temp = new File("C://Games", "temp");
-        if (temp.mkdir()) {
-            str.append("Yes\n");
-        }
-
-        File srcMain = new File("C://Games//src", "main");
-        if (srcMain.mkdir()) {
-            str.append("Yes\n");
-        }
-
-        File srcTest = new File("C://Games//src", "test");
-        if (srcTest.mkdir()) {
-            str.append("Yes\n");
-        }
-
-        File newMain = new File("C://Games//src//main", "Main.java");
-        try {
-            if (newMain.createNewFile())
-                str.append("Create\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        File newUtils = new File("C://Games//src//main", "Utils.java");
-        try {
-            if (newUtils.createNewFile())
-                str.append("Create\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        File drawables = new File("C://Games//res", "drawables");
-        if (drawables.mkdir()) {
-            str.append("Yes\n");
-        }
-
-        File vectors = new File("C://Games//res", "vectors");
-        if (vectors.mkdir()) {
-            str.append("Yes\n");
-        }
-
-        File icons = new File("C://Games//res", "icons");
-        if (icons.mkdir()) {
-            str.append("Yes\n");
-        }
-
-        File newTemp = new File("C://Games//temp", "temp.txt");
-        try {
-           if (newTemp.createNewFile())
-               str.append("Create\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try(FileWriter writer = new FileWriter("C://Games//temp//temp.txt", false)) {
+        try (FileWriter writer = new FileWriter("C://Games//temp//temp.txt", false)) {
             writer.write(String.valueOf(str));
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void newDir(String path, String name) {
+        File file = new File(path, name);
+        if (file.mkdir()) {
+            str.append("Done newDir!\n");
+        }
+    }
+
+    public static void newFile(String path, String name) {
+        File file = new File(path, name);
+        try {
+            if (file.createNewFile())
+                str.append("Done newFile!\n");
+        } catch (IOException e) {
+            str.append("Done newDir!\n");
         }
     }
 }
